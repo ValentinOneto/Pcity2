5c2e9602bfcf9fa6fa638b77bb99c9ace078077c
Julian <49548508@est.ort.edu.ar>
2024-09-26 09:11:33 -0300

    busqueda fachera

---
+++ COMPARACION-NEW/main.js
@@ -0,0 +1,35 @@
+const opcions = ["skibidi", "toilet", "sigma","yakatai","aieka","alezito","ñomñom","mi pueblo no se toca"];
+const input1 = document.getElementById('input1');
+const busq1 = document.getElementById('busq1');
+
+const input2 = document.getElementById('input2');
+const busq2 = document.getElementById('busq2');
+
+setupAutocomplete(input1, busq1, opcions);
+setupAutocomplete(input2, busq2, opcions);
+
+function setupAutocomplete(input, busq, options) {
+    input.addEventListener('input', function() {
+        const inputValue = input.value.toLowerCase();
+        busq.innerHTML = '';
+
+        const opcionFilt = options.filter(opcion =>
+            opcion.toLowerCase().startsWith(inputValue)
+        );
+
+        opcionFilt.forEach(opcion => {
+            const suggestionItem = document.createElement('div');
+            suggestionItem.textContent = opcion;
+            busq.appendChild(suggestionItem);
+
+            suggestionItem.addEventListener('click', function() {
+                input.value = opcion;
+                busq.innerHTML = '';
+            });
+        });
+
+        if (inputValue === '' || opcionFilt.length === 0) {
+            busq.innerHTML = '';
+        }
+    });
+}
