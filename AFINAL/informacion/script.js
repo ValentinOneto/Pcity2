ee69db04d54c58042e2a959d7172cf2399b5ca23
Julian <49548508@est.ort.edu.ar>
2024-10-07 11:03:04 -0300

    nose

--- AFINAL/informacion/script.js
+++ AFINAL/informacion/script.js
@@ -1,32 +1,35 @@
-const options = ["aiekas","skibidi", "toilet", "sigma","yakatai","aieka","alezito","ñomñom","mi pueblo no se toca"];
-const input1 = document.getElementById('input1');
-const busq1 = document.getElementById('busq1');
+import { onEvent, sendEvent, startServer } from "soquetic";
 
+document.addEventListener('DOMContentLoaded', () => {
+    const options = ["aiekas","skibidi", "toilet", "sigma","yakatai","aieka","alezito","ñomñom","mi pueblo no se toca"];
+    const input1 = document.getElementById('input1');
+    const busq1 = document.getElementById('busq1');
 
-function setupAutocomplete(input, busq, options) {
-    input.addEventListener('input', function() {
-        const inputValue = input.value.toLowerCase();
-        busq.innerHTML = '';
-
-        const opcionFilt = options.filter(opcion =>
-            opcion.toLowerCase().startsWith(inputValue)
-        );
-
-        opcionFilt.forEach(opcion => {
-            const suggestionItem = document.createElement('div');
-            suggestionItem.textContent = opcion;
-            busq.appendChild(suggestionItem);
-
-            suggestionItem.addEventListener('click', function() {
-                input.value = opcion;
-                busq.innerHTML = '';
-            });
-        });
-
-        if (inputValue === '' || opcionFilt.length === 0) {
+    function setupAutocomplete(input, busq, options) {
+        input.addEventListener('input', function() {
+            const inputValue = input.value.toLowerCase();
             busq.innerHTML = '';
-        }
-    });
-}
 
-setupAutocomplete(input1, busq1, options);
+            const opcionFilt = options.filter(opcion =>
+                opcion.toLowerCase().startsWith(inputValue)
+            );
+
+            opcionFilt.forEach(opcion => {
+                const suggestionItem = document.createElement('div');
+                suggestionItem.textContent = opcion;
+                busq.appendChild(suggestionItem);
+
+                suggestionItem.addEventListener('click', function() {
+                    input.value = opcion;
+                    busq.innerHTML = '';
+                });
+            });
+
+            if (inputValue === '' || opcionFilt.length === 0) {
+                busq.innerHTML = '';
+            }
+        });
+    }
+
+    setupAutocomplete(input1, busq1, options);
+});
