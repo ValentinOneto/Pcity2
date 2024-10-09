9e6cdbd62f389ff5123bc04032a3f1b4deb898f2
Bautista2049 <49379221@est.ort.edu.ar>
2024-10-09 13:50:14 -0300

    Tia paola 2

--- back/server.js
+++ back/server.js
@@ -3,3 +3,10 @@ import fs from "fs"
 
 let datos = fs.readFileSync("Datos.json", "utf8")
 console.log(datos);
+
+let Login = fs.readFileSync("Login.json", "utf8")
+console.log(Login);
+
+let Pcs = fs.readFileSync("Pcs.json", "utf8")
+console.log(Pcs);
+
