3b2ae2da48a1f34c6a04ca9b71fc9aa7356e45f8
Bautista2049 <49379221@est.ort.edu.ar>
2024-10-16 13:26:50 -0300

    q

---
+++ back/script.js
@@ -0,0 +1,13 @@
+import { onEvent, sendEvent, startServer } from "soquetic";
+import fs from "fs"
+
+let datos = fs.readFileSync("Datos.json", "utf8")
+console.log(datos);
+
+let Login = fs.readFileSync("Login.json", "utf8")
+console.log(Login);
+
+let Pcs = fs.readFileSync("Pcs.json", "utf8")
+console.log(Pcs);
+
+onEvent()
