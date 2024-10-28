ec7a3a2c8061c96f511f104c7368821036c05f0d
Julian <49548508@est.ort.edu.ar>
2024-10-28 15:56:28 -0300

    avance login

--- AFINAL/login/main.js
+++ AFINAL/login/main.js
@@ -1,11 +1,23 @@
-const iniciar = document.getElementById("iniciar");
-const registrarse = document.getElementById("regist");
+const iniciarBoton = document.getElementById("iniciar");
+const registrarBoton = document.getElementById("registrar");
+
+
 const nombreInput = document.getElementById("nombre");
 const contraseñaInput = document.getElementById("contra");
+const nombreInput2 = document.getElementById("nombre2");
+const contraseñaInput2 = document.getElementById("contra2");
+
+const login = document.getElementById("login");
+const signup = document.getElementById('signup');
+
 const error = document.getElementById("error");
 
+const botonCrear = document.getElementById("crear");
+const crearCuenta = document.getElementById("crearCuenta");
+const iniciarSesion = document.getElementById("inicarSesion");
 
-function inicarSesion(){
+
+function iniciarSesion(){
     let nombre = nombreInput.value;
     let contraseña = contraseñaInput.value;
     
@@ -13,9 +25,27 @@ function inicarSesion(){
         if (response.ok){
             window.location.href = "../inicio/index.html"
         } else {
-            error.classList.remove("err")
             error.classList.add("error")
         }
     })
+}
+
+iniciarBoton.addEventListener("click", iniciarSesion);
+
+function crearCuentas(){
+    
+    nombreInput.classList.add("non");
+    contraseñaInput.classList.add("non");
+
+    signup.classList.remove("non");
+
+    botonCrear.classList.add("non");
+    crearCuenta.classList.add("non");
+    iniciarBoton.classList.add("non");
+
+    registrarBoton.classList.remove("non");
+    iniciarSesion.classList.remove("non");
 
 }
+
+botonCrear.addEventListener("click", crearCuentas);
