460321bf43890eda9a07097d68a76248ef26068d
Bautista2049 <49379221@est.ort.edu.ar>
2024-10-28 09:22:39 -0300

    aaa

--- back/script.js
+++ back/script.js
@@ -14,3 +14,16 @@ console.log(Pcs);
 onEvent("Datos", datosfun)
 onEvent("Login", loginfun)
 onEvent("Pcs", pcsfun)
+
+onEvent("Datos", (data) => {
+    const usuarios
+    fs.writeFile("Login.json", JSON.stringify(usuarios, null, 2));
+  });
+
+  onEvent("Login", (data) => {
+    
+  });
+
+  onEvent("Pcs", (data) => {
+    
+  });
