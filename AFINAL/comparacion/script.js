ebeb4e1bae33a3dceef3b0dd3cddbbae4cf9942b
Julian <49548508@est.ort.edu.ar>
2024-11-05 11:21:26 -0300

    un poco de comparacion para haer resultados

--- AFINAL/comparacion/script.js
+++ AFINAL/comparacion/script.js
@@ -1,3 +1,21 @@
-postData('componentes', componentesSeleccionados, (todosLosComponentes) => {
+import { append } from "vary";
+
+let componentes;
+
+fetchData('componentes', (todosLosComponentes) => {
+    componentes = todosLosComponentes;
 });
 
+
+let selects = document.querySelectorAll('.select');
+    selects.forEach(select => {  
+        componentes.forEach(componente => {
+            let opcion = document.createElement('option').innerHTML;
+            let componente = opcion;
+            select.appendChild(componente);
+    })
+});
+
+postData('envioComponentes', componentesSeleccionados,() => {
+
+})
